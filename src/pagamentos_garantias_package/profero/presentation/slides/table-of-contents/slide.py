23e0b37171bff03418936b6171594f5716715ea3
williamroque <william.aroque@gmail.com>
2020-10-30 12:15:37 -0400

    Added table of contents. Started work on dados da operacao slide.

--- src/pagamentos_garantias_package/profero/presentation/slides/table-of-contents/slide.py
+++ src/pagamentos_garantias_package/profero/presentation/slides/table-of-contents/slide.py
@@ -1,9 +1,69 @@
-from pptx.util import Cm
+from pptx.util import Cm, Pt
+from pptx.enum.shapes import MSO_SHAPE
+from pptx.enum.text import PP_ALIGN, MSO_ANCHOR
+from pptx.dml.color import RGBColor
 
 from profero.framework.presentation.slide import Slide as FSlide
 from profero.framework.presentation.row import Row
+from profero.framework.presentation.cell import Cell
 from profero.presentation.slides.common.header import HeaderRow
 
+import re
+
+
+class EntryCell(Cell):
+    def __init__(self, inputs, slide_width, parent_row):
+        super().__init__(
+            inputs,
+            {
+                'width': slide_width,
+                'x_offset': 0
+            },
+            'entry', 0,
+            parent_row
+        )
+
+        self.text = ''
+
+    def render(self, slide):
+        box_width = self.width * .87
+        box_height = self.parent_row.height * .9
+
+        self.entry_box = self.create_rect(
+            self.x_offset + self.width / 2 - box_width / 2,
+            self.parent_row.y_offset + self.parent_row.height / 2 - box_height / 2,
+            box_width, box_height
+        )
+        self.set_shape_transparency(self.entry_box, 100)
+
+    def add_entry(self, title, pages):
+        CHARS_PER_ROW = 113
+
+        pages.sort()
+
+        page_str = ''
+        for i in range(len(pages)):
+            if i > 0:
+                page_str += (',' if pages[i] - pages[i-1] > 1 else '-') + str(pages[i])
+            else:
+                page_str += str(pages[i])
+
+        page_str = re.sub(r'(\d+-)(\d+-)+', r'\1', page_str)
+
+        self.text += '\n{}{}'.format(
+            title,
+            page_str.rjust(CHARS_PER_ROW - len(title), '.')
+        )
+
+        self.set_text(
+            self.entry_box,
+            self.text,
+            font_family='Courier',
+            font_size=Pt(12),
+            color=RGBColor(0x20, 0x38, 0x64),
+            alignment=PP_ALIGN.JUSTIFY,
+            vertical_anchor=MSO_ANCHOR.TOP
+        )
 
 class Slide(FSlide):
     def __init__(self, inputs, index, props, parent_presentation):
@@ -29,3 +89,21 @@ class Slide(FSlide):
             self
         )
         self.add_row(header_row)
+
+        entry_row = Row(
+            inputs,
+            {
+                'height': .75 * slide_height,
+                'y_offset': header_row.y_offset + header_row.height
+            },
+            'entry', 1,
+            self
+        )
+
+        entry_cell = EntryCell(inputs, slide_width, entry_row)
+
+        entry_row.add_cell(entry_cell)
+
+        self.add_row(entry_row)
+
+
--- src/pagamentos_garantias_package/test.js
+++ src/pagamentos_garantias_package/test.js
@@ -20,6 +20,33 @@ const input = {
         {
             id: 'table-of-contents',
             inputs: {}
+        },
+        {
+            id: 'dados-operacao',
+            inputs: {
+                '16': {
+                    'instrumento-financeiro': '18C0722274',
+                    'isin': 'BRLGOSCRI0D1',
+                    'cedente': 'Paysage',
+                    'correcao': 'IPCA',
+                    'juros': .085
+                    'data-emissao': '12/03/2020',
+                    'vencimento': '07/08/2026',
+                    'valor-emissao': 25.9 * 1_000_000,
+                    'saldo-devedor': 14.02 * 1_000_000
+                },
+                '17': {
+                    'instrumento-financeiro': '18C0722263',
+                    'isin': 'BRLGOSCRI0E9',
+                    'cedente': 'Paysage',
+                    'correcao': 'IPCA',
+                    'juros': .135
+                    'data-emissao': '12/03/2020',
+                    'vencimento': '07/05/2031',
+                    'valor-emissao': 11.1 * 1_000_000,
+                    'saldo-devedor': 8.73 * 1_000_000
+                }
+            }
         }
     ]
 };
